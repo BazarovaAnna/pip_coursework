ec3006b8b004f5e25549ad36fe4b806d8f310dff
danila <dds2412@mail.ru>
2018-11-17 12:36:26 +0300

    Добавена сущность Effect со всеми полями. Добавлен базовый rest api для сущности Effect (Добавление нового эффекта, получение всех эффектов). Обработано исключение одинаковых логинов пользователей. Добавлен EffectRepository с методом поиска по названию эффекта. Добавлена проверка одинаковых имен до добавления в БД.

---
+++ src/main/java/com/pip_coursework/entity/Effect.java
@@ -0,0 +1,32 @@
+package com.pip_coursework.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Effects")
+public class Effect {
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EffectsSequence")
+    private  long id;
+
+    @Column(name = "Name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "Description")
+    private String description;
+
+    protected Effect(){
+
+    }
+
+    public Effect(String name, String description){
+        this.name = name;
+        this.description= description;
+    }
+
+    @Override
+    public String toString() {
+        return  String.format("%s Effect name - '%s'",id, name);
+    }
+}
