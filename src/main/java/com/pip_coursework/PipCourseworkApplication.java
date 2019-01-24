8a6229ca141863637cfbba533e6c360712d5364f
danila <dds2412@mail.ru>
2019-01-24 06:53:16 +0300

    Добавлены некоторые параметры запуска

--- src/main/java/com/pip_coursework/PipCourseworkApplication.java
+++ src/main/java/com/pip_coursework/PipCourseworkApplication.java
@@ -2,17 +2,10 @@ package com.pip_coursework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PipCourseworkApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PipCourseworkApplication.class);
-    }
-
-    public static void main(String[] args) throws Exception {
+public class PipCourseworkApplication {
+    public static void main(String[] args) {
         SpringApplication.run(PipCourseworkApplication.class, args);
     }
 }
